af00b32f1feab17787f3e1ad0a0236ffcd65a0a7
yasaichi <yasaichi@users.noreply.github.com>
2018-03-23 00:50:20 +0900

    4.2.2

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+)
+
+var tlds = []string{"com", "net"}
+
+const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		text := strings.ToLower(s.Text())
+		var newText []rune
+
+		// NOTE: `r` is Rune type
+		for _, r := range text {
+			if unicode.IsSpace(r) {
+				r = '-'
+			}
+			if !strings.ContainsRune(allowedChars, r) {
+				continue
+			}
+
+			newText = append(newText, r)
+		}
+
+		fmt.Println(string(newText) + "." + tlds[rand.Intn(len(tlds))])
+	}
+	if err := s.Err(); err != nil {
+		log.Fatalln(err)
+	}
+}
